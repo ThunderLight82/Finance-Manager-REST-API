e319d47e9d059b028692db2940a8353e0ce34a91
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:09:33 +0200

    Added [FinancialOperation.cs] to hold entire operation base including fields like "DateTime", "Amount" and type of operation

---
+++ FinanceManagerAPI.Domain/Models/FinancialOperation.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceManagerAPI.Domain.Models;
+
+public class FinancialOperation : BaseModel
+{
+    public DateTime DateTime { get; set; }
+
+    public decimal Amount { get; set; }
+
+    public int OperationTypeId { get; set; }
+    
+    public OperationType? OperationType { get; set; }
+}
