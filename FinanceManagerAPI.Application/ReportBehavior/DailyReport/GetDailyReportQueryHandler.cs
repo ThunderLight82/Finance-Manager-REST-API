8dca60e36c25b74fdb5ff6f537bcc0a110dde0ff
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:35:28 +0200

    Added "Get" operation for "DailyReport" feature with appropriate validation

---
+++ FinanceManagerAPI.Application/ReportBehavior/DailyReport/GetDailyReportQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using FinanceManagerAPI.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FinanceManagerAPI.Application.ReportBehavior.DailyReport;
+
+internal class GetDailyReportQueryHandler : IRequestHandler<GetDailyReportQuery, DailyReportResponse>
+{
+    private readonly FinanceAPIDbContext _dbContext;
+    private readonly ILogger<GetDailyReportQueryHandler> _logger;
+
+    public GetDailyReportQueryHandler(FinanceAPIDbContext dbContext, ILogger<GetDailyReportQueryHandler> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<DailyReportResponse> Handle(GetDailyReportQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var financialOperations = await _dbContext.FinancialOperations
+                .Where(fo => fo.DateTime.Date == request.InputDate.Date)
+                .Include(fo => fo.OperationType)
+                .ToListAsync(cancellationToken);
+            
+            if (financialOperations == null)
+            {
+                _logger.LogError("Error in  GetDailyReportQueryHandler." +
+                                 "Financial operation with this date not found.");
+                throw new Exception("Financial operation with this date not found.");
+            }
+            
+            decimal totalIncome = financialOperations
+                .Where(fo => fo.OperationType!.IsIncomeOperation)
+                .Sum(fo => fo.Amount);
+        
+            decimal totalExpenses = financialOperations
+                .Where(fo => !fo.OperationType!.IsIncomeOperation)
+                .Sum(fo => fo.Amount);
+            
+            return new DailyReportResponse(totalIncome, totalExpenses, financialOperations);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error occurred while generating daily report: {ex.Message}");
+            throw;
+        }
+    }
+}
---
+++ FinanceManagerAPI.Application/ReportBehavior/DailyReport/GetDailyReportQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace FinanceManagerAPI.Application.ReportBehavior.DailyReport;
+
+public class GetDailyReportQueryValidator : AbstractValidator<GetDailyReportQuery>
+{
+    public GetDailyReportQueryValidator()
+    {
+        RuleFor(query => query.InputDate)
+            .NotNull().WithMessage("Error: Value [DateTime] shouldn't be null.")
+            .NotEmpty().WithMessage("Error: Value [DateTime] shouldn't be empty or 0.");
+    }
+}
