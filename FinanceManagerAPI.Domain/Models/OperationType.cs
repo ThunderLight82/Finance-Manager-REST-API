1d7955f1188b67e606a0895e68a4ad59a581aa16
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:10:01 +0200

    Added [OperationType.cs] hold additional info about fin.operation and have field like "Name" and operation type

---
+++ FinanceManagerAPI.Domain/Models/OperationType.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceManagerAPI.Domain.Models;
+
+public class OperationType : BaseModel
+{
+    public string? Name { get; set; }
+    
+    public bool IsIncomeOperation { get; set; }
+}
