a57c030354b680bc28cbd42dee9a6e6877920e5c
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:35:56 +0200

    Added "Get" operation for "DatePeriod" feature with appropriate validation

---
+++ FinanceManagerAPI.Application/ReportBehavior/DatePeriodReport/GetDatePeriodReportQueryHandler.cs
@@ -0,0 +1,51 @@
+﻿using FinanceManagerAPI.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FinanceManagerAPI.Application.ReportBehavior.DatePeriodReport;
+
+internal class GetDatePeriodReportQueryHandler : IRequestHandler<GetDatePeriodReportQuery, DatePeriodResponse>
+{
+    private readonly FinanceAPIDbContext _dbContext;
+    private readonly ILogger<GetDatePeriodReportQueryHandler> _logger;
+
+    public GetDatePeriodReportQueryHandler(FinanceAPIDbContext dbContext, ILogger<GetDatePeriodReportQueryHandler> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<DatePeriodResponse> Handle(GetDatePeriodReportQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var financialOperations = await _dbContext.FinancialOperations
+                .Where(fo => fo.DateTime >= request.StartInputDate && fo.DateTime <= request.EndInputDate)
+                .Include(fo => fo.OperationType)
+                .ToListAsync(cancellationToken);
+            
+            if (financialOperations == null)
+            {
+                _logger.LogError("Error in  GetDatePeriodReportQueryHandler." +
+                                 "Financial operations with this date not found.");
+                throw new Exception("Financial operations with this date not found.");
+            }
+            
+            decimal totalIncome = financialOperations
+                .Where(fo => fo.OperationType!.IsIncomeOperation)
+                .Sum(fo => fo.Amount);
+        
+            decimal totalExpenses = financialOperations
+                .Where(fo => !fo.OperationType!.IsIncomeOperation)
+                .Sum(fo => fo.Amount);
+            
+            return new DatePeriodResponse(totalIncome, totalExpenses, financialOperations);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error occurred while fetching date period report: {ex.Message}");
+            throw;
+        }
+    }
+}
---
+++ FinanceManagerAPI.Application/ReportBehavior/DatePeriodReport/GetDatePeriodReportQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace FinanceManagerAPI.Application.ReportBehavior.DatePeriodReport;
+
+public class GetDatePeriodReportQueryValidator : AbstractValidator<GetDatePeriodReportQuery>
+{
+    public GetDatePeriodReportQueryValidator()
+    {
+        RuleFor(query => query.StartInputDate)
+            .NotNull().WithMessage("Error: Starting value [DateTime] shouldn't be null.")
+            .NotEmpty().WithMessage("Error: Starting value [DateTime] shouldn't be empty or 0.");
+        
+        RuleFor(query => query.EndInputDate)
+            .NotNull().WithMessage("Error: Ending value [DateTime] shouldn't be null.")
+            .NotEmpty().WithMessage("Error: Ending value [DateTime] shouldn't be empty or 0.");
+    }
+}
