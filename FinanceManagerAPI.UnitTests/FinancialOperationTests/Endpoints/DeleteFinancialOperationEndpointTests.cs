1e609cc0f7374947dcbece96f83b0113b51c51c4
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:44:19 +0200

    Added some test for Endpoints in FinancialOperation using functional testing with HttpClient module

---
+++ FinanceManagerAPI.UnitTests/FinancialOperationTests/Endpoints/DeleteFinancialOperationEndpointTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using FinanceManagerAPI.DTO.ModelsDTOs;
+using FinanceManagerAPI.WebApi;
+using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Xunit;
+
+namespace FinanceManagerAPI.UnitTests.FinancialOperationTests.Endpoints;
+
+public class DeleteFinancialOperationEndpointTests
+{
+    private readonly TestServer _testServer;
+    private readonly HttpClient _httpClient;
+
+    public DeleteFinancialOperationEndpointTests()
+    {
+        _testServer = new TestServer(new WebHostBuilder()
+            .ConfigureServices(services =>
+                {
+                    services.AddRouting();
+                })
+            .UseStartup<Startup>()
+            .UseSerilog());
+
+        _httpClient = _testServer.CreateClient();
+    }
+    
+    [Fact]
+    public async Task DeleteFinancialOperationEndpoint_EndpointValidation_InvalidRequests_ShouldReturnBadRequest()
+    { 
+        // Arrange
+        var financialOperationDto = new FinancialOperationDto { Id = default };
+
+        // Act
+        var response = await _httpClient.DeleteAsync($"api/financial-operation/{financialOperationDto.Id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+}
