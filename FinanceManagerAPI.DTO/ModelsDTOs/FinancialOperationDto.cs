470655663ba203164050b0ee392b0f86f53b8b5b
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:15:28 +0200

    Created [FinancialOperationDto.cs] object for same purpose like in Domain layer

---
+++ FinanceManagerAPI.DTO/ModelsDTOs/FinancialOperationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceManagerAPI.DTO.ModelsDTOs;
+
+public class FinancialOperationDto: BaseModelDto
+{
+    public DateTime DateTime { get; init; }
+    
+    public decimal Amount { get; init; }
+    
+    public int OperationTypeDtoId { get; init; }
+}
