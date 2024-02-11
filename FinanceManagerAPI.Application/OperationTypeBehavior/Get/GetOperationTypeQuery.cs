8916d2782c2d39dbd44308a7ebfaa4e85bb0fa14
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:34:27 +0200

    Added "Get" operation for "OperationType" with appropriate validation

---
+++ FinanceManagerAPI.Application/OperationTypeBehavior/Get/GetOperationTypeQuery.cs
@@ -0,0 +1,8 @@
+﻿using FinanceManagerAPI.DTO.ModelsDTOs;
+using MediatR;
+
+namespace FinanceManagerAPI.Application.OperationTypeBehavior.Get;
+
+public record GetOperationTypeQuery(int OperationTypeId) :  IRequest<OperationTypeDto>;
+
+
---
+++ FinanceManagerAPI.Application/OperationTypeBehavior/Get/GetOperationTypeQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using FinanceManagerAPI.DataAccess;
+using FinanceManagerAPI.DTO.ModelsDTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FinanceManagerAPI.Application.OperationTypeBehavior.Get;
+
+internal class GetOperationTypeQueryHandler : IRequestHandler<GetOperationTypeQuery, OperationTypeDto>
+{
+    private readonly FinanceAPIDbContext _dbContext;
+    private readonly ILogger<GetOperationTypeQueryHandler> _logger;
+
+    public GetOperationTypeQueryHandler(FinanceAPIDbContext dbContext, ILogger<GetOperationTypeQueryHandler> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<OperationTypeDto> Handle(GetOperationTypeQuery request, CancellationToken cancellationToken)
+    {
+        var operationTypeId = request.OperationTypeId;
+
+        var getOperationType = await _dbContext.OperationsTypes
+            .Where(ot => ot.Id == operationTypeId)
+            .FirstOrDefaultAsync(cancellationToken);
+            
+        if (getOperationType == null)
+        {
+            _logger.LogError($"Error in OperationTypeBehavior - Get." +
+                             $"Operation type with ID {request.OperationTypeId} not found.");
+            throw new Exception($"Operation type with ID {request.OperationTypeId} not found.");
+        }
+
+        return new OperationTypeDto
+        {
+            Id = getOperationType.Id,
+            Name = getOperationType.Name,
+            IsIncomeOperation = getOperationType.IsIncomeOperation
+        };
+    }
+}
