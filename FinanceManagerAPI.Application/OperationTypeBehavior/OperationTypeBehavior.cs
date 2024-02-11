8de13593ac64c87cb78b2a7b9061634fecfc2f7d
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:31:28 +0200

    Created a folder to contain handler logic for OperationType model.
    Added a specific class, OperationTypeBehavior.cs, responsible for registering in Dependency Injection and sharing the database context among all handlers.

---
+++ FinanceManagerAPI.Application/OperationTypeBehavior/OperationTypeBehavior.cs
@@ -0,0 +1,9 @@
+﻿using FinanceManagerAPI.DataAccess;
+using FinanceManagerAPI.Domain.Models;
+
+namespace FinanceManagerAPI.Application.OperationTypeBehavior;
+
+internal class OperationTypeBehavior : BaseBehavior<OperationType>
+{
+    public OperationTypeBehavior(FinanceAPIDbContext dbContext) : base(dbContext) { }
+}
