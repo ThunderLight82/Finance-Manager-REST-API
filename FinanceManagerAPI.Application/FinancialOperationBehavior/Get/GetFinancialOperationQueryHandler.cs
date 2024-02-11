b759cec45dc8122593fda02b8cb8c052fe95b44c
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:33:34 +0200

    Added "Get" operation for "FinancialOperation" with appropriate validation

---
+++ FinanceManagerAPI.Application/FinancialOperationBehavior/Get/GetFinancialOperationQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using FinanceManagerAPI.DataAccess;
+using FinanceManagerAPI.DTO.ModelsDTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace FinanceManagerAPI.Application.FinancialOperationBehavior.Get;
+
+internal class GetFinancialOperationQueryHandler : IRequestHandler<GetFinancialOperationQuery, FinancialOperationDto>
+{
+    private readonly FinanceAPIDbContext _dbContext;
+    private readonly ILogger<GetFinancialOperationQueryHandler> _logger;
+
+    public GetFinancialOperationQueryHandler(FinanceAPIDbContext dbContext, ILogger<GetFinancialOperationQueryHandler> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task<FinancialOperationDto> Handle(GetFinancialOperationQuery request, CancellationToken cancellationToken)
+    {
+        var financialOperationId = request.FinancialOperationId;
+
+        var getFinancialOperation = await _dbContext.FinancialOperations
+            .Where(fo => fo.Id == financialOperationId)
+            .FirstOrDefaultAsync(cancellationToken);
+            
+        if (getFinancialOperation == null)
+        {
+            _logger.LogError($"Error in FinancialOperationBehavior - Get." +
+                             $"Financial operation with ID {request.FinancialOperationId} not found.");
+            throw new ArgumentNullException($"Financial operation with ID {request.FinancialOperationId} not found.");
+        }
+
+        return new FinancialOperationDto
+        {
+            Id = getFinancialOperation.Id,
+            DateTime = getFinancialOperation.DateTime,
+            Amount = getFinancialOperation.Amount,
+            OperationTypeDtoId = getFinancialOperation.OperationTypeId
+        };
+    }
+}
