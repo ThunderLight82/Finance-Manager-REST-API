61e5a96dae8be0441b9351a3195a9b8341b9db7e
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:33:20 +0200

    Added "Delete" operation for "FinancialOperation" with appropriate validation

---
+++ FinanceManagerAPI.Application/FinancialOperationBehavior/Delete/DeleteFinancialOperationCommand.cs
@@ -0,0 +1,6 @@
+﻿using FinanceManagerAPI.DTO.ModelsDTOs;
+using MediatR;
+
+namespace FinanceManagerAPI.Application.FinancialOperationBehavior.Delete;
+
+public record DeleteFinancialOperationCommand(FinancialOperationDto FinancialOperationDto) : IRequest<Unit> { }
