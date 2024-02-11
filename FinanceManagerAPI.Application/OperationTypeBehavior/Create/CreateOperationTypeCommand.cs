363fd74f3ec4bca750bb89730e1fc127c9c522d5
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:34:06 +0200

    Added "Create" operation for "OperationType" with appropriate validation

---
+++ FinanceManagerAPI.Application/OperationTypeBehavior/Create/CreateOperationTypeCommand.cs
@@ -0,0 +1,6 @@
+﻿using FinanceManagerAPI.DTO.ModelsDTOs;
+using MediatR;
+
+namespace FinanceManagerAPI.Application.OperationTypeBehavior.Create;
+
+public record CreateOperationTypeCommand(OperationTypeDto OperationTypeDto) : IRequest<Unit> { }
---
+++ FinanceManagerAPI.Application/OperationTypeBehavior/Create/CreateOperationTypeCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace FinanceManagerAPI.Application.OperationTypeBehavior.Create;
+
+public class CreateOperationTypeCommandValidator : AbstractValidator<CreateOperationTypeCommand>
+{
+    public CreateOperationTypeCommandValidator()
+    {
+        RuleFor(command => command.OperationTypeDto.Name)
+            .NotNull().WithMessage("Error: Value [Name] shouldn't be null.")
+            .NotEmpty().WithMessage("Error: Value [Name] shouldn't be empty or whitespace.");
+        
+        RuleFor(command => command.OperationTypeDto.IsIncomeOperation)
+            .NotNull().WithMessage("Error: Value [IsIncomeOperation] shouldn't be null.")
+            .IsInEnum().WithMessage("Error: Value [IsIncomeOperation] should be a valid boolean.");
+    }
+}
