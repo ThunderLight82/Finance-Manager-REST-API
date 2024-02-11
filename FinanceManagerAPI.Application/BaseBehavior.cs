b155b28eaad9e66c8755aee3625e63ea37815bac
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-11 20:27:38 +0200

    Added 2 crucial classes:
    1. [BaseBehavior.cs] Represents repositories and UnitOfWork functionalities. This class provides methods for CRUD operations and managing the database context.
    2. [DependencyInjection.cs] to Implements dependency injection configuration for the application layer. Registers necessary services such as MediatR handlers and validators.

---
+++ FinanceManagerAPI.Application/BaseBehavior.cs
@@ -0,0 +1,43 @@
+﻿using FinanceManagerAPI.Application.Interfaces;
+using FinanceManagerAPI.DataAccess;
+using FinanceManagerAPI.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceManagerAPI.Application;
+
+internal class BaseBehavior<TBaseModel> : IBaseBehavior<TBaseModel> where TBaseModel : BaseModel
+{
+    private readonly FinanceAPIDbContext _dbContext;
+
+    protected BaseBehavior(FinanceAPIDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public virtual async Task<TBaseModel> GetById(int id) =>
+        (await _dbContext.Set<TBaseModel>().FindAsync(id))!;
+
+    public virtual async Task<IEnumerable<TBaseModel>> GetAll() =>
+        await _dbContext.Set<TBaseModel>().ToListAsync();
+
+    public virtual async Task Create(TBaseModel model) => 
+        await _dbContext.Set<TBaseModel>().AddAsync(model);
+
+    public virtual Task Delete(TBaseModel model)
+    {
+        _dbContext.Set<TBaseModel>().Remove(model);
+
+        return Task.CompletedTask;
+    }
+
+    public virtual Task Update(TBaseModel model)
+    {
+        _dbContext.Entry(model).State = EntityState.Modified;
+        
+        return Task.CompletedTask;
+    }
+
+    //UnitOfWork SaveChanges is declared here instead of in DataAccess layer.
+    public virtual async Task SaveChangesAsync(CancellationToken cancellationToken) => 
+        await _dbContext.SaveChangesAsync(cancellationToken);
+}
