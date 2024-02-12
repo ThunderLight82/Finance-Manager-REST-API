f3c7c9f433af23f3252a7fffb7d2b1e8c1e22fad
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-12 15:53:53 +0200

    Another fix in exception type

--- FinanceManagerAPI.Application/ReportBehavior/DailyReport/GetDailyReportQueryHandler.cs
+++ FinanceManagerAPI.Application/ReportBehavior/DailyReport/GetDailyReportQueryHandler.cs
@@ -24,12 +24,12 @@ internal class GetDailyReportQueryHandler : IRequestHandler<GetDailyReportQuery,
                 .Where(fo => fo.DateTime.Date == request.InputDate.Date)
                 .Include(fo => fo.OperationType)
                 .ToListAsync(cancellationToken);
-            
-            if (financialOperations == null)
+
+            if (financialOperations.Count == 0)
             {
                 _logger.LogError("Error in  GetDailyReportQueryHandler." +
                                  "Financial operation with this date not found.");
-                throw new ArgumentNullException($"Financial operation with this date '{request.InputDate}' not found.");
+                throw new InvalidOperationException($"Financial operation with this date '{request.InputDate}' not found.");
             }
             
             decimal totalIncome = financialOperations
