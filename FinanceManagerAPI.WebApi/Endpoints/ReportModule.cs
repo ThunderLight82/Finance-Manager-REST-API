4f3ff5bff4fd5fbc656749d17be8d283cfbb9c69
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-12 17:59:45 +0200

    Created [ReportModule.cs] and implemented endpoint logic for the daily report feature and date period feature.

---
+++ FinanceManagerAPI.WebApi/Endpoints/ReportModule.cs
@@ -0,0 +1,68 @@
+﻿using Carter;
+using FinanceManagerAPI.Application.ReportBehavior.DailyReport;
+using FinanceManagerAPI.Application.ReportBehavior.DatePeriodReport;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceManagerAPI.WebApi.Endpoints;
+
+public class ReportModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder endpoint)
+    {
+        var mapGroup = endpoint.MapGroup("api/report");
+
+        mapGroup.MapGet("daily/{date}", GetDailyReport).CacheOutput();
+        mapGroup.MapGet("date-period/{startDate}/{endDate}", GetDatePeriodReport).CacheOutput();
+    }
+    
+    // [Get]
+    public static async Task<IResult> GetDailyReport(
+        [FromRoute] DateTime date,
+        IValidator<GetDailyReportQuery> validator,
+        ISender sender)
+    {
+        var query = new GetDailyReportQuery(date);
+            
+        var validationResult = await validator.ValidateAsync(query);
+        if (!validationResult.IsValid)
+        {
+            return TypedResults.ValidationProblem(validationResult.ToDictionary());
+        }
+            
+        try
+        {
+            return TypedResults.Ok(await sender.Send(query));
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.NotFound($"Error 404: {ex.Message}");
+        }
+    }
+    
+    // [Get]
+    public static async Task<IResult> GetDatePeriodReport(
+        [FromRoute] DateTime startDate,
+        [FromRoute] DateTime endDate,
+        IValidator<GetDatePeriodReportQuery> validator,
+        ISender sender)
+    {
+        var query = new GetDatePeriodReportQuery(startDate, endDate);
+            
+        var validationResult = await validator.ValidateAsync(query);
+        if (!validationResult.IsValid)
+        {
+            return TypedResults.ValidationProblem(validationResult.ToDictionary());
+        }
+            
+        try
+        {
+            return TypedResults.Ok(await sender.Send(query));
+        }
+        catch (Exception ex)
+        {
+            return TypedResults.NotFound($"Error 404: {ex.Message}");
+        }
+    }
+}
